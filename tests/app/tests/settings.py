a761eaca57b3740e2ea9ef0763921d9285524a6c
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2015-05-17 23:54:13 +0300

    Added test for default settings filepath in django 1.6 production project has error

---
+++ tests/app/tests/settings.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from mtr.sync.settings import FILE_PATH
+
+
+class MockInstance(object):
+    action = 1
+
+
+class SettingsTest(TestCase):
+
+    def test_default_filepath_name(self):
+        new_settings = {
+            'MEDIA_ROOT': '/some/path/'
+        }
+        instance = MockInstance()
+
+        with self.settings(**new_settings):
+            self.assertEqual(
+                FILE_PATH()(instance, 'filename.xls'),
+                'sync/import/filename.xls')
