6664766878cade83acf0c648e0e1573d16dee7bb
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2015-08-26 22:34:41 +0300

    Added tests for inner manager keys

--- mtr/utils/manager.py
+++ mtr/utils/manager.py
@@ -9,24 +9,24 @@ class Manager(object):
         self._registered = {}
         self._imported = False
 
-    def get(self, type_name, func_name, inner=None):
+    def get(self, type_name, func_name, related=None):
         funcs = self._registered.get(type_name, {})
-        if inner and funcs:
-            funcs = funcs.get(inner, {})
+        if related and funcs:
+            funcs = funcs.get(related, {})
         return funcs.get(func_name, None)
 
     def _register_dict(
-            self, type_name, func_name, label, inner=None, **kwargs):
+            self, type_name, func_name, label, related=None, **kwargs):
         """Return decorator for adding functions as key, value
         to instance, dict"""
 
         def decorator(func):
             key = type_name
-            outer = None
             values = self._registered.get(key, OrderedDict())
-            if inner:
+            outer = None
+            if related:
                 outer = values
-                values = values.get(inner, OrderedDict())
+                values = values.get(related, OrderedDict())
             position = \
                 getattr(func, 'position', 0) or kwargs.get('position', 0)
             new_name = func_name or func.__name__
@@ -41,8 +41,8 @@ class Manager(object):
                     sorted(
                         values.items(),
                         key=lambda p: getattr(p[1], 'position', 0)))
-            if inner and outer:
-                outer[key] = inner
+            if related:
+                outer[related] = values
                 values = outer
             self._registered[key] = values
 
@@ -52,18 +52,20 @@ class Manager(object):
 
     def register(
             self, type_name, label=None, name=None,
-            item=None, inner=None, **kwargs):
+            item=None, **kwargs):
         """Decorator and function to config new handlers"""
 
         func = self._register_dict(type_name, name, label, **kwargs)
 
         return func(item) if item else func
 
-    def unregister(self, type_name, item=None, inner=None):
+    def unregister(self, type_name, item=None, related=None):
         """Decorator to pop dict items"""
 
         items = self._registered.get(type_name, None)
-        if items is not None:
+        if items:
+            if related:
+                items = items.get(related, {})
             items.pop(getattr(item, '__name__', item), None)
 
         return item
--- mtr/utils/tests/manager.py
+++ mtr/utils/tests/manager.py
@@ -36,3 +36,32 @@ class ManagerTest(TestCase):
 
         self.assertNotIn('test', self.manager._registered['item'])
         self.assertNotIn(somefunc, self.manager._registered['item'].values())
+
+    def test_register_unregister_inner(self):
+        @self.manager.register('item', related='asd')
+        def somefunc(some, args):
+            print(some, args)
+
+        @self.manager.register('item', name='other', related='asd')
+        def someotherfunc(some, args):
+            print(some, args)
+
+        self.assertIn(
+            somefunc.__name__, self.manager._registered['item']['asd'])
+        self.assertIn(
+            somefunc, self.manager._registered['item']['asd'].values())
+
+        self.assertIn('other', self.manager._registered['item']['asd'])
+        self.assertIn(
+            someotherfunc, self.manager._registered['item']['asd'].values())
+
+        self.assertEqual(
+            somefunc, self.manager.get('item', 'somefunc', related='asd'))
+        self.assertEqual(
+            someotherfunc, self.manager.get('item', 'other', related='asd'))
+
+        self.manager.unregister('item', somefunc, related='asd')
+
+        self.assertNotIn('test', self.manager._registered['item']['asd'])
+        self.assertNotIn(
+            somefunc, self.manager._registered['item']['asd'].values())
