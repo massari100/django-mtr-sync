de587d0df5e662726160fd02b80cd72481c7fa60
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2015-10-18 23:47:00 +0300

    Removed comments from settings

--- tests/app/settings.py
+++ tests/app/settings.py
@@ -1,12 +1,3 @@
-"""
-Django settings for app project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
 import os
 
 import django
@@ -15,17 +6,10 @@ import django
 # placeholder for gettext
 def _(s): return s
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'j#zwt2c!*(7(jz!m(tr$+jq^1d(+)e(^059f^nd_(*zj!gv0x)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
@@ -37,7 +21,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'modeltranslation',
-    # 'grappelli',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -108,10 +91,6 @@ ROOT_URLCONF = 'app.urls'
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -121,9 +100,6 @@ DATABASES = {
 
 if django.get_version() >= '1.7':
     DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en'
 
@@ -140,16 +116,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Celery settings
 BROKER_BACKEND = 'memory'
 
 CELERY_ACCEPT_CONTENT = ['json']
