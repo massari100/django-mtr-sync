7e7ff4250bf0570b8bcb6c43f8f03074cebd2d12
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2016-04-15 22:13:27 +0300

    Removed i18n pattern to fix strange namescape admin error

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sync/', include('mtr.sync.urls')),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(
